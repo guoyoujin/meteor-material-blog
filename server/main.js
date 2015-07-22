0d2b50691bf6e21eba5d46c5a834799ff6811142
guoyoujin <guoyj@tongxinyiliao.com>
2015-07-23 00:28:18 +0800

    汉化

--- server/main.js
+++ server/main.js
@@ -1,15 +1,14 @@
 // Uncomment the code below and fill in your information.
 // Then start Meteor in your project to create an initial admin user
 
-/*
+
 if (Meteor.users.find().count() === 0) {
   var userObj = {
-      username: 'admin', 
-      email: 'admin@example.com', 
-      password: 'admin',
-      profile: {name: 'Admin'},
+      username: 'guoyoujin', 
+      email: 'guoyoujin123@gmail.com', 
+      password: 'guoyoujin',
+      profile: {name: '郭友进'},
       roles: 'admin',
     };
   Accounts.createUser(userObj);
 }
-*/
